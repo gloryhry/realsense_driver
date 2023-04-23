2d5ec0554bbb405bb6586e7d616f369810be926f
glory <gloryhry@qq.com>
2023-04-23 16:42:48 +0800

    重构，支持多相机同时输出color、深度、红外图

--- include/realsense_driver.h
+++ include/realsense_driver.h
@@ -1,28 +1,28 @@
 /*
- * _______________#########_______________________ 
- * ______________############_____________________ 
- * ______________#############____________________ 
- * _____________##__###########___________________ 
- * ____________###__######_#####__________________ 
- * ____________###_#######___####_________________ 
- * ___________###__##########_####________________ 
- * __________####__###########_####_______________ 
- * ________#####___###########__#####_____________ 
- * _______######___###_########___#####___________ 
- * _______#####___###___########___######_________ 
- * ______######___###__###########___######_______ 
- * _____######___####_##############__######______ 
- * ____#######__#####################_#######_____ 
- * ____#######__##############################____ 
- * ___#######__######_#################_#######___ 
- * ___#######__######_######_#########___######___ 
- * ___#######____##__######___######_____######___ 
- * ___#######________######____#####_____#####____ 
- * ____######________#####_____#####_____####_____ 
- * _____#####________####______#####_____###______ 
- * ______#####______;###________###______#________ 
- * ________##_______####________####______________ 
- * 
+ * _______________#########_______________________
+ * ______________############_____________________
+ * ______________#############____________________
+ * _____________##__###########___________________
+ * ____________###__######_#####__________________
+ * ____________###_#######___####_________________
+ * ___________###__##########_####________________
+ * __________####__###########_####_______________
+ * ________#####___###########__#####_____________
+ * _______######___###_########___#####___________
+ * _______#####___###___########___######_________
+ * ______######___###__###########___######_______
+ * _____######___####_##############__######______
+ * ____#######__#####################_#######_____
+ * ____#######__##############################____
+ * ___#######__######_#################_#######___
+ * ___#######__######_######_#########___######___
+ * ___#######____##__######___######_____######___
+ * ___#######________######____#####_____#####____
+ * ____######________#####_____#####_____####_____
+ * _____#####________####______#####_____###______
+ * ______#####______;###________###______#________
+ * ________##_______####________####______________
+ *
  * @Author: Glory Huang
  * @Date: 2023-04-18 20:27:11
  * @LastEditors: Glory Huang
@@ -42,31 +42,149 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
+#include <thread>
+
+#define Align_To_Color 0
+#define Align_To_Infrared 1
 
 class Camera
 {
 public:
-    Camera()
+    Camera() {}
+    Camera(int number_, std::string serial_, bool color_, bool depth_, bool infrared_, int align_, ros::NodeHandle nh_)
     {
-        ros::NodeHandle nh;
-        ros::NodeHandle private_nh("~");
-        std::string color_topic, depth_topic, infrared_topic;
-        private_nh.param<std::string>("color_topic", color_topic, "/camera/color/image_raw");
-        private_nh.param<std::string>("depth_topic", depth_topic, "/camera/depth/image_raw");
-        private_nh.param<std::string>("infrared_topic", infrared_topic, "/camera/infrared/image_raw");
+        nh = nh_;
+        camera_number = number_;
+        camera_serial_number = serial_;
+        color = color_;
+        depth = depth_;
+        infrared = infrared_;
+        align_to = align_;
+        pipe_config.enable_device(camera_serial_number);
+        if (color)
+        {
+            pipe_config.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
+        }
+        if (depth)
+        {
+            pipe_config.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_RGB8, 30);
+        }
+        if (infrared)
+        {
+            pipe_config.enable_stream(RS2_STREAM_INFRARED, 640, 480, RS2_FORMAT_Y8, 30);
+        }
 
+        profile = pipe.start(pipe_config);
+        if (depth)
+        {
+            auto depth_stream = profile.get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>();
+            const auto depth_intrinsics = depth_stream.get_intrinsics();
+            float depth_scale = get_depth_scale(profile.get_device());
+            std::cout << "Camera" << camera_number << " depth_scale = " << depth_scale << std::endl;
+        }
+        if (color)
+        {
+            auto color_stream = profile.get_stream(RS2_STREAM_COLOR).as<rs2::video_stream_profile>();
+            auto color_intrinsics = color_stream.get_intrinsics();
+        }
+        if (infrared)
+        {
+            auto infrared_stream = profile.get_stream(RS2_STREAM_INFRARED).as<rs2::video_stream_profile>();
+            auto infrared_intrinsics = infrared_stream.get_intrinsics();
+        }
+
+        // 选择彩色图像数据流来作为对齐对象
+
+        // 对齐的是彩色图，所以彩色图是不变的
+        if (align_to == Align_To_Color)
+        {
+            aligned = RS2_STREAM_COLOR;
+        }
+        if (align_to == Align_To_Infrared)
+        {
+            aligned = RS2_STREAM_INFRARED;
+        }
+        // 将深度图对齐到RGB图
+        align = rs2::align(aligned);
+    }
+    void set_Camera(int number_, std::string serial_, bool color_, bool depth_, bool infrared_, int align_,ros::NodeHandle nh_)
+    {
+        nh = nh_;
+        camera_number = number_;
+        camera_serial_number = serial_;
+        color = color_;
+        depth = depth_;
+        infrared = infrared_;
+        align_to = align_;
+        pipe_config.enable_device(camera_serial_number);
+        if (depth)
+        {
+            pipe_config.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
+        }
+        if (color)
+        {
+            pipe_config.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_RGB8, 30);
+        }
+        if (infrared)
+        {
+            pipe_config.enable_stream(RS2_STREAM_INFRARED, 640, 480, RS2_FORMAT_Y8, 30);
+        }
+
+        profile = pipe.start(pipe_config);
+        if (depth)
+        {
+            auto depth_stream = profile.get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>();
+            const auto depth_intrinsics = depth_stream.get_intrinsics();
+            float depth_scale = get_depth_scale(profile.get_device());
+            std::cout << "Camera" << camera_number << " depth_scale = " << depth_scale << std::endl;
+        }
+        if (color)
+        {
+            auto color_stream = profile.get_stream(RS2_STREAM_COLOR).as<rs2::video_stream_profile>();
+            auto color_intrinsics = color_stream.get_intrinsics();
+        }
+        if (infrared)
+        {
+            auto infrared_stream = profile.get_stream(RS2_STREAM_INFRARED).as<rs2::video_stream_profile>();
+            auto infrared_intrinsics = infrared_stream.get_intrinsics();
+        }
+
+        // 选择彩色图像数据流来作为对齐对象
+        // 对齐的是彩色图，所以彩色图是不变的
+        if (align_to == Align_To_Color)
+        {
+            aligned = RS2_STREAM_COLOR;
+        }
+        if (align_to == Align_To_Infrared)
+        {
+            aligned = RS2_STREAM_INFRARED;
+        }
+        // 将深度图对齐到RGB图
+        align = rs2::align(aligned);
+    }
+    void set_color_pub(std::string color_topic)
+    {
         color_pub = nh.advertise<sensor_msgs::Image>(color_topic, 1);
+        color_init = true;
+    }
+    void set_depth_pub(std::string depth_topic)
+    {
         depth_pub = nh.advertise<sensor_msgs::Image>(depth_topic, 1);
+        depth_init = true;
+    }
+    void set_infrared_pub(std::string infrared_topic)
+    {
         infrared_pub = nh.advertise<sensor_msgs::Image>(infrared_topic, 1);
+        infrared_init = true;
     }
     ~Camera() {}
 
 public:
-    ros::Publisher color_pub;
-    ros::Publisher depth_pub;
-    ros::Publisher infrared_pub;
-
-    rs2::context ctx;
+    ros::NodeHandle nh;
+    int align_to = 0;
+    bool color = true, depth = false, infrared = false;
+    int camera_number;
+    std::string camera_serial_number;
     // Declare depth colorizer for pretty visualization of depth data
     rs2::colorizer color_map;
     // Declare rates printer for showing streaming rates of the enabled streams.
@@ -74,10 +192,16 @@ public:
     // Declare RealSense pipeline, encapsulating the actual device and sensors
     rs2::pipeline pipe;
     rs2::config pipe_config;
-
+    rs2::pipeline_profile profile;
+    rs2_stream aligned;
+    rs2::align align = rs2::align(RS2_STREAM_COLOR);
     cv::Mat color_raw;
     cv::Mat depth_raw;
     cv::Mat infrared_raw;
+    ros::Publisher color_pub;
+    ros::Publisher depth_pub;
+    ros::Publisher infrared_pub;
+    bool color_init = false,depth_init=false,infrared_init=false;
 
 public:
     float get_depth_scale(rs2::device dev)
@@ -111,42 +235,6 @@ public:
     {
         try
         {
-            rs2::log_to_console(RS2_LOG_SEVERITY_ERROR);
-
-            auto devs = ctx.query_devices();
-            int device_num = devs.size();
-            std::cout << "获取相机设备号：" << device_num << std::endl;                                   // Device amount
-            rs2::device dev = *devs.begin();
-            std::cout << "rs2_camera_name： " << dev.get_info(RS2_CAMERA_INFO_NAME) << std::endl;                   //相机名字
-            std::cout << "rs2_camera_serial_number： " << dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER) << std::endl; //序列号
-            std::cout << "rs2_firmware_version： " << dev.get_info(RS2_CAMERA_INFO_FIRMWARE_VERSION) << std::endl;  //固件版本
-
-            pipe_config.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
-            pipe_config.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_RGB8, 30);
-            pipe_config.enable_stream(RS2_STREAM_INFRARED, 640, 480, RS2_FORMAT_Y8, 30);
-            // Start streaming with default recommended configuration
-            // The default video configuration contains Depth and Color streams
-            // If a device is capable to stream IMU data, both Gyro and A   ccelerometer are enabled by default
-            rs2::pipeline_profile profile = pipe.start(pipe_config);
-            auto depth_stream = profile.get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>();
-            const auto depth_intrinsics = depth_stream.get_intrinsics();
-
-            auto color_stream = profile.get_stream(RS2_STREAM_COLOR).as<rs2::video_stream_profile>();
-            auto color_intrinsics = color_stream.get_intrinsics();
-
-            auto infrared_stream = profile.get_stream(RS2_STREAM_INFRARED).as<rs2::video_stream_profile>();
-            auto infrared_intrinsics = infrared_stream.get_intrinsics();
-
-            float depth_scale = get_depth_scale(profile.get_device());
-            std::cout << "depth_scale = " << depth_scale << std::endl;
-
-            // 选择彩色图像数据流来作为对齐对象
-            rs2_stream align_to = RS2_STREAM_INFRARED; // 对齐的是彩色图，所以彩色图是不变的
-            // 将深度图对齐到RGB图
-            rs2::align align(align_to);
-
-            const auto window_name = "Display Image";
-            cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
             while (ros::ok())
             {
                 rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
@@ -156,36 +244,49 @@ public:
                 {
                     // 如果profile发生改变，则更新align对象，重新获取深度图像像素到长度单位的转换比例
                     profile = pipe.get_active_profile();
-                    align = rs2::align(align_to);
-                    depth_scale = get_depth_scale(profile.get_device());
+                    align = rs2::align(aligned);
+                    float depth_scale = get_depth_scale(profile.get_device());
                 }
                 // 获取对齐后的帧
                 auto processed = align.process(data);
+
                 // 尝试获取对齐后的深度图像帧和其他帧
-                rs2::frame aligned_color_frame = processed.get_color_frame();                         // RGB图
-                rs2::frame aligned_depth_frame = processed.get_depth_frame();                         // 深度图
-                rs2::frame aligned_infrared_frame = processed.get_infrared_frame();                   // 红外图
+                rs2::frame aligned_color_frame, aligned_depth_frame, aligned_infrared_frame;
+                if (color)
+                {
+                    aligned_color_frame = processed.get_color_frame(); // RGB图
+                }
+                if (depth)
+                {
+                    aligned_depth_frame = processed.get_depth_frame(); // 深度图
+                }
+                if (infrared)
+                {
+                    aligned_infrared_frame = processed.get_infrared_frame(); // 红外图
+                }
                 // 获取对齐之前的color图像
                 // rs2::frame before_depth_frame = data.get_depth_frame().apply_filter(color_map); // 获取对齐之前的深度图
 
                 // Query frame size (width and height)
-                const int color_w = aligned_color_frame.as<rs2::video_frame>().get_width();
-                const int color_h = aligned_color_frame.as<rs2::video_frame>().get_height();
-                const int depth_w = aligned_depth_frame.as<rs2::video_frame>().get_width();
-                const int depth_h = aligned_depth_frame.as<rs2::video_frame>().get_height();
-                // const int b_color_w = before_depth_frame.as<rs2::video_frame>().get_width();
-                // const int b_color_h = before_depth_frame.as<rs2::video_frame>().get_height();
-                const int infrared_w = aligned_infrared_frame.as<rs2::video_frame>().get_width();
-                const int infrared_h = aligned_infrared_frame.as<rs2::video_frame>().get_height();
-                // 如果其中一个未能获取，继续迭代
-                if (!aligned_depth_frame || !aligned_color_frame || !aligned_infrared_frame)
+                int color_w, color_h, depth_w, depth_h, infrared_w, infrared_h;
+                if (aligned_color_frame)
                 {
-                    continue;
+                    color_w = aligned_color_frame.as<rs2::video_frame>().get_width();
+                    color_h = aligned_color_frame.as<rs2::video_frame>().get_height();
+                    color_raw = cv::Mat(cv::Size(color_w, color_h), CV_8UC3, (void *)aligned_color_frame.get_data(), cv::Mat::AUTO_STEP);
                 }
-                // Create OpenCV matrix of size (w,h) from the colorized depth data
-                color_raw = cv::Mat(cv::Size(color_w, color_h), CV_8UC3, (void *)aligned_color_frame.get_data(), cv::Mat::AUTO_STEP);
-                depth_raw = cv::Mat(cv::Size(depth_w, depth_h), CV_16UC1, (void *)aligned_depth_frame.get_data(), cv::Mat::AUTO_STEP);
-                infrared_raw = cv::Mat(cv::Size(infrared_w, infrared_h), CV_8UC1, (void *)aligned_infrared_frame.get_data());
+                if (aligned_depth_frame)
+                {
+                    depth_w = aligned_depth_frame.as<rs2::video_frame>().get_width();
+                    depth_h = aligned_depth_frame.as<rs2::video_frame>().get_height();
+                    depth_raw = cv::Mat(cv::Size(depth_w, depth_h), CV_16UC1, (void *)aligned_depth_frame.get_data(), cv::Mat::AUTO_STEP);
+                }
+                if (aligned_infrared_frame)
+                {
+                    infrared_w = aligned_infrared_frame.as<rs2::video_frame>().get_width();
+                    infrared_h = aligned_infrared_frame.as<rs2::video_frame>().get_height();
+                    infrared_raw = cv::Mat(cv::Size(infrared_w, infrared_h), CV_8UC1, (void *)aligned_infrared_frame.get_data());
+                }
 
                 // // PointCloud
                 // // 创建一个深度帧和一个内参结构体
@@ -199,14 +300,33 @@ public:
                 // rs2_deproject_pixel_to_point(point, &intrin, pixel, depth_value);
 
                 std_msgs::Header header;
-                header.frame_id = "camera";
                 header.stamp = now_time;
-                sensor_msgs::ImagePtr color_msg = cv_bridge::CvImage(header, "rgb8", color_raw).toImageMsg();
-                sensor_msgs::ImagePtr depth_msg = cv_bridge::CvImage(header, "mono16", depth_raw).toImageMsg();
-                sensor_msgs::ImagePtr infrared_msg = cv_bridge::CvImage(header, "mono8", infrared_raw).toImageMsg();
-                color_pub.publish(*color_msg);
-                depth_pub.publish(*depth_msg);
-                infrared_pub.publish(*infrared_msg);
+
+                if (color && color_init)
+                {
+                    header.frame_id = "camera_color";
+                    sensor_msgs::ImagePtr color_msg = cv_bridge::CvImage(header, "rgb8", color_raw).toImageMsg();
+                    color_pub.publish(*color_msg);
+                }
+                if (depth && depth_init)
+                {
+                    header.frame_id = "camera_depth";
+                    sensor_msgs::ImagePtr depth_msg = cv_bridge::CvImage(header, "mono16", depth_raw).toImageMsg();
+                    depth_pub.publish(*depth_msg);
+                }
+                if (infrared && infrared_init)
+                {
+                    header.frame_id = "camera_infrared";
+                    sensor_msgs::ImagePtr infrared_msg = cv_bridge::CvImage(header, "mono8", infrared_raw).toImageMsg();
+                    infrared_pub.publish(*infrared_msg);
+                }
+
+                // sensor_msgs::ImagePtr color_msg = cv_bridge::CvImage(header, "rgb8", color_raw).toImageMsg();
+                // sensor_msgs::ImagePtr depth_msg = cv_bridge::CvImage(header, "mono16", depth_raw).toImageMsg();
+                // sensor_msgs::ImagePtr infrared_msg = cv_bridge::CvImage(header, "mono8", infrared_raw).toImageMsg();
+                // color_pub.publish(*color_msg);
+                // depth_pub.publish(*depth_msg);
+                // infrared_pub.publish(*infrared_msg);
             }
             return EXIT_SUCCESS;
         }
--- src/realsense_driver_node.cpp
+++ src/realsense_driver_node.cpp
@@ -1,14 +1,14 @@
 /*
- *           佛曰:  
- *                   写字楼里写字间，写字间里程序员；  
- *                   程序人员写程序，又拿程序换酒钱。  
- *                   酒醒只在网上坐，酒醉还来网下眠；  
- *                   酒醉酒醒日复日，网上网下年复年。  
- *                   但愿老死电脑间，不愿鞠躬老板前；  
- *                   奔驰宝马贵者趣，公交自行程序员。  
- *                   别人笑我忒疯癫，我笑自己命太贱；  
+ *           佛曰:
+ *                   写字楼里写字间，写字间里程序员；
+ *                   程序人员写程序，又拿程序换酒钱。
+ *                   酒醒只在网上坐，酒醉还来网下眠；
+ *                   酒醉酒醒日复日，网上网下年复年。
+ *                   但愿老死电脑间，不愿鞠躬老板前；
+ *                   奔驰宝马贵者趣，公交自行程序员。
+ *                   别人笑我忒疯癫，我笑自己命太贱；
  *                   不见满街漂亮妹，哪个归得程序员？
- * 
+ *
  * @Author: Glory Huang
  * @Date: 2023-04-18 20:08:32
  * @LastEditors: Glory Huang
@@ -20,19 +20,64 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
-#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API 
+#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include <realsense_driver.h>
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "kinco_node");
-    // pkg_loc = ros::package::getPath("kinco_driver");
-    
+    ros::init(argc, argv, "realsense_driver");
+    ros::NodeHandle nh;
+    ros::NodeHandle private_nh("~");
 
-    
-    Camera d430;
-    d430.rs_imshow();
+    std::string color_topic, depth_topic, infrared_topic;
+    bool enable_color, enable_depth, enable_infrared;
+    std::string camera_serials;
+    int camera_number;
+    private_nh.param<std::string>("color_topic", color_topic, "/color/image_raw");
+    private_nh.param<std::string>("depth_topic", depth_topic, "/depth/image_raw");
+    private_nh.param<std::string>("infrared_topic", infrared_topic, "/infrared/image_raw");
+    private_nh.param<bool>("enable_color", enable_color, true);
+    private_nh.param<bool>("enable_depth", enable_depth, false);
+    private_nh.param<bool>("enable_infrared", enable_infrared, false);
+    private_nh.param<std::string>("camera_serials", camera_serials, "");
+    private_nh.param<int>("camera_number", camera_number, 0);
 
-    // ros::spin();
+    color_topic = "/camera" + std::to_string(camera_number) + color_topic;
+    depth_topic = "/camera" + std::to_string(camera_number) + depth_topic;
+    infrared_topic = "/camera" + std::to_string(camera_number) + infrared_topic;
+
+    ros::Publisher color_pub;
+    ros::Publisher depth_pub;
+    ros::Publisher infrared_pub;
+    color_pub = nh.advertise<sensor_msgs::Image>(color_topic, 1);
+    depth_pub = nh.advertise<sensor_msgs::Image>(depth_topic, 1);
+    infrared_pub = nh.advertise<sensor_msgs::Image>(infrared_topic, 1);
+
+    rs2::log_to_console(RS2_LOG_SEVERITY_WARN);
+    rs2::context ctx;
+    auto devs = ctx.query_devices();
+    int device_num = devs.size();
+    std::cout << "获取到相机设备数量：" << device_num << std::endl; // Device amount
+
+    for (auto dev : devs)
+    {
+        std::cout << "-------------------" << std::endl;
+        std::cout << "rs2_camera_name： " << dev.get_info(RS2_CAMERA_INFO_NAME) << std::endl;                   // 相机名字
+        std::cout << "rs2_camera_serial_number： " << dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER) << std::endl; // 序列号
+        std::cout << "rs2_firmware_version： " << dev.get_info(RS2_CAMERA_INFO_FIRMWARE_VERSION) << std::endl;  // 固件版本
+    }
+    rs2::device dev = *devs.begin();
+
+    Camera cam;
+    if (camera_serials == "")
+    {
+        camera_serials = dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER);
+    }
+    cam.set_Camera(camera_number, camera_serials, enable_color, enable_depth, enable_infrared, Align_To_Color, nh);
+    cam.set_color_pub(color_topic);
+    cam.set_depth_pub(depth_topic);
+    cam.set_infrared_pub(infrared_topic);
+    cam.rs_imshow();
+
     return 0;
 }
